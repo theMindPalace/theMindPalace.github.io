3a15cc5c2e0f7cf20465416be0ee2edbabfc921c
Sarath <sarath.hacks@gmail.com>
2016-07-09 14:15:39 +0530

    Text effect

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+ $(function(){
+        $(".element").typed({
+            strings: ["#theMindPalace", "M1ND P4LAC3","COMING SOON..."],
+            typeSpeed: 0,
+            loop:true
+        });
+ });
